11773e09a93521cb9a27cf46e2cb890b91c08715
stacru <hamaw8888@gmail.com>
2020-08-26 20:56:29 +0900

    linkのサンプルを作成

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Index() {
-  return <div>Welcome to Next.js!</div>
+import Link from 'next/link'
+
+const Index = () => {
+  return (
+    <>
+      <div>Welcome to Next.js!</div>
+      <Link href="/linkSample">
+        <a>link Sample Page</a>
+      </Link>
+    </>
+  )
 }
 
 export default Index
